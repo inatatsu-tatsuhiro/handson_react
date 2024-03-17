5d2b41985b5ba8e0e59d49f98809a0903d78e813
inatatsu <tatsuhiro.9699@gmail.com>
2024-03-17 22:23:26 +0900

    :sparkles: add mysql

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,28 @@
 import express from "express";
 import cors from "cors";
+import mysql from "mysql";
 const app: express.Express = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const logData = `
+2023/12/19 13:01:12 leave 教員森山 1moriyama7 リアル研究室
+2023/12/19 13:43:10 join B4_堀0121 b4_hori リアル研究室
+2023/12/19 14:01:41 leave B4堀0121 b4_hori リアル研究室
+2023/12/19 14:01:42 join B4堀0121 b4_hori リアル研究室
+2023/12/19 14:03:23 message b4_hori データベースにログデータのinser作業
+2023/12/19 14:20:30 join M2塩野 M2_塩野0482 バーチャル個室01
+2023/12/19 14:20:45 leave M2塩野 M2_塩野0482 バーチャル個室01
+2023/12/19 14:59:30 join M2福永0455 hikaru5469 バーチャル会議室
+2023/12/19 14:59:37 join 教員森山 1moriyama7 バーチャル会議室
+2023/12/19 15:01:07 message 1moriyama7 院生の文献紹介です。
+2023/12/19 15:01:13 message hikaru5469 院生、文献紹介
+2023/12/19 15:03:19 join M2塩野 M2塩野0482 バーチャル会議室
+2023/12/20 8:25:54 join M2塩野 M2_塩野0482 バーチャル個室01
+`;
 
 const json = {
   name: "タスクボード",
@@ -78,10 +95,60 @@ const updateTask = (id: string) => {
 };
 
 app.get("/", (_req, res) => {
-  res.send("Hello World!");
+  const time1 = "2023/12/19-12:24:16";
+  const time2 = "2023/12/19-12:25:06";
+  const date1 = new Date(time1).getTime();
+  const date2 = new Date(time2).getTime();
+  res.send({ sub: date2 - date1, date1, date2 });
 });
+
+app.get("/log", (_req, res) => {
+  const logs = logData.split("\n");
+  const removedMessage = [];
+  const result = [];
+  for (const l of logs) {
+    if (l.indexOf("message") === -1) {
+      removedMessage.push(l);
+    }
+  }
+
+  const insertedJoin = [];
+
+  for (const l of removedMessage) {
+    if (l === "") continue;
+    if (l.indexOf("join")) {
+      console.log({ l });
+      const tmp = l.split(" ");
+      const j = {
+        join_at: `${tmp[0]}-${tmp[1]}`,
+        leave_at: `${tmp[0]}-${tmp[1]}`,
+        user_id: tmp[4],
+      };
+      insertedJoin.push(j);
+    }
+  }
+  // for (const l of logs) {
+  //   if (l.indexOf("leave")) {
+  //     const tmp = l.split(" ");
+  //     const j = {
+  //       join_at: `${tmp[0]}-${tmp[1]}`,
+  //       leave_at: `${tmp[0]}-${tmp[1]}`,
+  //       user_id: tmp[4],
+  //     };
+  //     insertedJoin.push(j);
+  //   }
+  // }
+
+  res.send({ result: insertedJoin });
+});
+
 app.get("/todos", (_req, res) => {
   res.send(json);
+});
+app.get("/todos/:id", (req, res) => {
+  res.send({
+    id: req.params.id,
+  });
 });
 
 app.post("/todos/create", (req, res) => {
