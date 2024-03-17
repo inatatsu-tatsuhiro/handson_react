9820e015230d918e91a1c6a2f0ac07762026f7fa
inatatsu <tatsuhiro.9699@gmail.com>
2024-03-17 22:13:47 +0900

    :sparkles: add mysql

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,29 +12,52 @@ const styles = {
     marginTop: '80px'
   })
 }
+type Log = {
+  join_at: string
+  leave_at: string
+  user_id: string
+}
+type Data = {
+  result: Log[]
+}
 
 function App() {
   const [taskBoard, setTaskBoard] = useState<Board | null>(null)
+  const [logs, setLogs] = useState<Log[]>([])
 
   const update = () => {
-    axios
-      .get('http://localhost:3000/todos')
-      .then((res: AxiosResponse<Board>) => {
-        console.log(res.data)
-        setTaskBoard(res.data)
-      })
+    // axios
+    //   .get('http://localhost:3000/todos')
+    //   .then((res: AxiosResponse<Board>) => {
+    //     console.log(res.data)
+    //     setTaskBoard(res.data)
+    //   })
   }
+
   useEffect(() => {
-    update()
+    // update()
+    axios.get('http://localhost:3000/log').then((res: AxiosResponse<Data>) => {
+      console.log(res.data)
+      setLogs(res.data.result)
+    })
   }, [])
 
-  if (taskBoard === null) {
-    return <Empty />
-  }
+  // if (taskBoard === null) {
+  //   return <Empty />
+  // }
 
   return (
     <div className={styles.root}>
-      <TaskBoard taskBoard={taskBoard} update={update} />
+      {/* <TaskBoard taskBoard={taskBoard} update={update} /> */}
+      <div>
+        {logs.map((log) => (
+          <div key={log.join_at}>
+            <div>{log.user_id}</div>
+            <div>{log.join_at}</div>
+            <div>{log.leave_at}</div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
